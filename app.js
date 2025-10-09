4b28e09bd591b188f3ed9358451105a21909f475
JonnyMeshuggah <48164930+JonnyMeshuggah@users.noreply.github.com>
2025-10-09 19:36:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,15 +7,7 @@ const SUBJECTS = [
 ];
 
 // ==== Firebase ====
-const firebaseConfig = {
-  apiKey: window.env.FIREBASE_API_KEY,
-  authDomain: window.env.FIREBASE_AUTH_DOMAIN,
-  projectId: window.env.FIREBASE_PROJECT_ID,
-  storageBucket: window.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: window.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: window.env.FIREBASE_APP_ID,
-  measurementId: window.env.FIREBASE_MEASUREMENT_ID
-};
+
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
